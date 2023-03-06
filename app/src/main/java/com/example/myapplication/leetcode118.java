ae5f6a90c804a8b4f2f0de10e9a5bf9bc8735431
zhouwenpeng <zhouwenpeng@yaofangwang.com>
2023-03-06 17:56:29 +0800

    两个动态规划

---
+++ app/src/main/java/com/example/myapplication/leetcode118.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class leetcode118 {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) {
+            return result;
+        } else {
+            List<Integer> list = new ArrayList<>();
+            list.add(1);
+            result.add(list);
+        }
+        for (int i = 2; i <= numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            List<Integer> last = result.get(i - 2);
+            for (int j = 0; j < i; j++) {
+                if (j == 0 || (j == i - 1)) {
+                    //第一位和最后一位肯定是1
+                    list.add(1);
+                } else {
+                    list.add(last.get(j - 1) + last.get(j));
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        generate(5);
+    }
+}
